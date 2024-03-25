6c45989030734908b675048f35c879a45acf0872
Michel Gutner <michel.gutner@sanar.com>
2024-03-25 08:28:49 -0300

    fix: add initial structure android player

--- android/app/src/main/java/com/rnvideoplayer/RNVideoPlayer.kt
+++ android/app/src/main/java/com/rnvideoplayer/RNVideoPlayer.kt
@@ -1,20 +1,49 @@
 package com.rnvideoplayer
-import android.util.Log
-import com.facebook.react.bridge.Callback
-import com.facebook.react.bridge.ReactApplicationContext
-import com.facebook.react.bridge.ReactContextBaseJavaModule
-import com.facebook.react.bridge.ReactMethod
+import android.graphics.Color
+import android.net.Uri
+import android.view.View
+import com.facebook.react.bridge.ReadableMap
+import com.facebook.react.uimanager.SimpleViewManager
+import com.facebook.react.uimanager.ThemedReactContext
+import com.facebook.react.uimanager.annotations.ReactProp
+import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.ui.PlayerView
 
-class RNVideoPlayer(reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext) {
+class RNVideoPlayer : SimpleViewManager<View>() {
+    private var exoPlayer: SimpleExoPlayer? = null
+  override fun getName() = "RNVideoPlayer"
 
+  override fun createViewInstance(reactContext: ThemedReactContext): PlayerView {
+    var playerView =  PlayerView(reactContext)
+    initializePlayer(reactContext, playerView)
+    return playerView
+  }
 
-    override fun getName() = "RNVideoPlayer"
+  @ReactProp(name = "color")
+  fun setColor(view: View, color: String) {
+    view.setBackgroundColor(Color.parseColor(color))
+  }
 
-
-    @ReactMethod
-    fun createCalendarEvent(name: String, location: String, callback: Callback) {
-        val eventId = 123
-        callback.invoke(eventId)
+  private fun initializePlayer(context: ThemedReactContext, playerView: PlayerView) {
+    if (exoPlayer == null) {
+      exoPlayer = SimpleExoPlayer.Builder(context).build()
     }
+
+    playerView.player = exoPlayer
+  }
+
+  @ReactProp(name = "source")
+  fun setSource(playerView: PlayerView, source: ReadableMap?) {
+    val url = source?.getString("url")
+    if (!url.isNullOrEmpty()) {
+      val uri = Uri.parse(url)
+      val mediaSource = MediaItem.fromUri(url)
+      playerView.player?.addMediaItem(mediaSource)
+      playerView.hideController()
+      playerView.player?.prepare()
+      playerView.player?.play()
+    }
+  }
 }
 
--- android/app/src/main/java/com/rnvideoplayer/RNVideoPlayerPackage.kt
+++ android/app/src/main/java/com/rnvideoplayer/RNVideoPlayerPackage.kt
@@ -1,19 +1,18 @@
 package com.rnvideoplayer
 
-import android.view.View
 import com.facebook.react.ReactPackage
 import com.facebook.react.bridge.NativeModule
 import com.facebook.react.bridge.ReactApplicationContext
-import com.facebook.react.uimanager.ReactShadowNode
 import com.facebook.react.uimanager.ViewManager
 
 class RNVideoPlayerPackage : ReactPackage {
 
-    override fun createViewManagers(
-            reactContext: ReactApplicationContext
-    ): MutableList<ViewManager<View, ReactShadowNode<*>>> = mutableListOf()
+  override fun createNativeModules(reactContext: ReactApplicationContext): List<NativeModule> {
+    return emptyList()
+  }
 
-    override fun createNativeModules(
-            reactContext: ReactApplicationContext
-    ): MutableList<NativeModule> = listOf(RNVideoPlayer(reactContext)).toMutableList()
+  override fun createViewManagers(reactContext: ReactApplicationContext): List<ViewManager<*, *>> {
+    return listOf(RNVideoPlayer())
+  }
+
 }
