74d0ec0c6950c2f5026fd400a6e6202406b2d840
Michel Gutner <michel.gutner@sanar.com>
2024-09-03 09:58:04 -0300

    feat: add components base

--- ComponentsBase/CircleButton.swift
+++ ComponentsBase/CircleButton.swift
@@ -6,3 +6,14 @@
 //
 
 import Foundation
+import SwiftUI
+
+func CreateCircleButton(action: @escaping () -> Void,_ overlay: some View) -> some View {
+    Button(action: action) {
+        Circle()
+            .fill(.gray.opacity(0.15))
+            .frame(width: 40, height: 40)
+            .overlay(overlay)
+            .padding(.all, 8)
+    }
+}
--- ComponentsBase/CircleMenuButton.swift
+++ ComponentsBase/CircleMenuButton.swift
@@ -6,3 +6,42 @@
 //
 
 import Foundation
+import SwiftUI
+
+@available(iOS 14.0, *)
+func CreateCircleMenuButton(menus: NSDictionary?, _ overlay: some View, action: @escaping (_ label: String, _ value: Any) -> Void) -> some View {
+    var transformedNSDictionaryIntoSwiftDictionary: [(key: String, values: [NSDictionary])] = []
+    
+    if let menus = menus {
+        transformedNSDictionaryIntoSwiftDictionary = menus.compactMap { (key, value) -> (key: String, values: [NSDictionary])? in
+            if let key = key as? String, let values = value as? [NSDictionary] {
+                return (key: key, values: values)
+            }
+
+            return nil
+        }
+    }
+
+    return Menu {
+        ForEach(transformedNSDictionaryIntoSwiftDictionary, id: \.key) { option in
+            Menu(option.key) {
+                ForEach(option.values, id: \.self) { item in
+                    Button(action: {
+                        let label = item["name"] as! String
+                        let value = item["value"] as Any
+                        action(option.key, value)
+                    }) {
+                        if let label = item["name"] as? String {
+                            Label(label, systemImage: "chevron.right")
+                        }
+                    }
+                }
+            }
+        }
+    } label: {
+        Circle()
+            .fill(Color.black.opacity(0.3))
+            .frame(width: 40, height: 40)
+            .overlay(overlay)
+    }
+}
