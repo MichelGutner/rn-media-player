048854707c8ebbfca29de07be2c8d557f8af143e
Michel Gutner <michel.gutner@sanar.com>
2024-09-10 00:13:45 -0300

    fix: helpers and utils

--- android/src/main/java/com/rnvideoplayer/utils/utils.kt
+++ android/src/main/java/com/rnvideoplayer/utils/utils.kt
@@ -5,24 +5,29 @@ import android.animation.AnimatorListenerAdapter
 import android.view.View
 
 fun View.fadeIn(duration: Long = 500) {
-  this.visibility = View.VISIBLE
-  this.alpha = 0f
-  this.animate()
-    .alpha(1f)
-    .setDuration(duration)
-    .setListener(null)
+  this.post {
+    this.visibility = View.VISIBLE
+    this.alpha = 0f
+    this.animate()
+      .alpha(1f)
+      .setDuration(duration)
+      .setListener(null)
+  }
 }
 
 fun View.fadeOut(duration: Long = 500) {
-  this.animate()
-    .alpha(0f)
-    .setDuration(duration)
-    .setListener(object : AnimatorListenerAdapter() {
-      override fun onAnimationEnd(animation: Animator) {
-        this@fadeOut.visibility = View.GONE
-      }
-    })
+  this.post {
+    this.animate()
+      .alpha(0f)
+      .setDuration(duration)
+      .setListener(object : AnimatorListenerAdapter() {
+        override fun onAnimationEnd(animation: Animator) {
+          this@fadeOut.visibility = View.GONE
+        }
+      })
+  }
 }
+
 
 fun scaleView(isForward: Boolean, view: View) {
   val scale = 2f
