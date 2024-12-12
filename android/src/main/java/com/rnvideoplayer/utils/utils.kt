6ccbdd5bcc414f5b74d34313e825c6b0438a4659
Michel Gutner <michel.gutner@sanar.com>
2024-12-12 10:20:39 -0300

    refactor: adjust scale view

--- android/src/main/java/com/rnvideoplayer/utils/utils.kt
+++ android/src/main/java/com/rnvideoplayer/utils/utils.kt
@@ -4,16 +4,16 @@ import android.view.View
 
 
 fun scaleView(isForward: Boolean, view: View) {
-  val playPauseButtonSize = 70f
+  val centerButton = 40f
   if (isForward) {
     view.pivotX = 0f
-    view.translationX = (view.width.toFloat() / 2f + playPauseButtonSize)
+    view.translationX = (view.width.toFloat() / 2f + centerButton)
   } else {
     view.pivotX = view.width.toFloat()
-    view.translationX = -(view.width.toFloat() / 2f + playPauseButtonSize)
+    view.translationX = -(view.width.toFloat() / 2f + centerButton)
   }
   view.pivotY = view.height / 2f
-  view.animate().scaleX(1f).scaleY(1f).setDuration(200).start()
+  view.animate().scaleX(1f).scaleY(1.2f).setDuration(200).start()
   view.requestLayout()
 }
 
